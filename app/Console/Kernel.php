bb854355ad475187a279b0dff27ba55a0fa4348a
daichongweb <daichongweb@foxmail.com>
2023-12-31 12:04:25 +0800

    feat: #1231 定时任务取消订单

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +14,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $orders = Order::query()->where('status', Order::STATUS_WAIT)
+                ->where('created_at', '<', Carbon::now()->subMinutes(15))
+                ->get();
+            $orders?->each(function ($order) {
+                $order->update(['status' => Order::STATUS_CACHE]);
+            });
+        })->everyMinute();
     }
 
     /**
@@ -20,7 +29,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
